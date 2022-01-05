ac6bf24ffcc2854753c726d0dad1f2618eabcb5b
huydevct <huyctdev@example.com>
2022-01-05 21:26:05 +0700

    Auth Guard

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,3 +1,5 @@
+import { GqlAuthGuard } from './gql-auth.guard';
+import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { LoginResponse } from './dto/login-response';
@@ -5,10 +7,11 @@ import { LoginUserInput } from './dto/login-user.input';
 
 @Resolver()
 export class AuthResolver {
-    constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
-    @Mutation(() => LoginResponse)
-    login(@Args('loginUserInput') loginUserInput: LoginUserInput){
-        return this.authService.login(loginUserInput);
-    }
+  @Mutation(() => LoginResponse)
+  @UseGuards(GqlAuthGuard)
+  login(@Args('loginUserInput') loginUserInput: LoginUserInput) {
+    return this.authService.login(loginUserInput);
+  }
 }
