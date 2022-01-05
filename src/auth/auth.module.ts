fcaeef8edc96622f1e6c9a470d157ce2b4825030
huydevct <huyctdev@example.com>
2022-01-05 21:12:05 +0700

    Login Mutation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,12 @@
+import { UsersModule } from './../users/users.module';
+import { LocalStrategy } from './local.strategy';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  providers: [AuthService, AuthResolver]
+  imports: [PassportModule, UsersModule],
+  providers: [AuthService, AuthResolver, LocalStrategy]
 })
 export class AuthModule {}
--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,6 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { LoginResponse } from './dto/login-response';
+import { LoginUserInput } from './dto/login-user.input';
 
 @Resolver()
 export class AuthResolver {
-    
+    constructor(private authService: AuthService){}
+
+    @Mutation(() => LoginResponse)
+    login(@Args('loginUserInput') loginUserInput: LoginUserInput){
+        return this.authService.login(loginUserInput);
+    }
 }
